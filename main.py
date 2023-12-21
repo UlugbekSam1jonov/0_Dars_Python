852a668b8824ef1aeb22e486db90d1d7b694d3a7
UlugbekSam1jonov <103875617+UlugbekSam1jonov@users.noreply.github.com>
2023-12-21 16:52:02 +0500

    Python kirish
    Python kirish 00dars

---
+++ main.py
@@ -0,0 +1,10 @@
+print('"Nexia" "Tico" "Damas" korganlar qilar havas')
+
+# 5ning 4chi darajasini qaytaradigan dastur
+print(5 ** 4)
+
+# 22ni 4bo'lganda qoldig'ini aniqlaydigan dastur
+print(22 % 4,"Qoldiq")
+
+print(12 * 3.14)
+print(6 * 7)
